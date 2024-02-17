c5061e59badafc9432ac6ce76e4c2f9bb53cd7c9
Mohammed Arham <arhamuddin0@gmail.com>
2024-02-17 19:24:12 +0530

    Code updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'xray',
+    version= '0.0.0',
+    author= 'Mohammed Arham',
+    author_email= 'arhamuddin0@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
